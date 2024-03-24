76d73d0c1e221478504bd82f93d774ba3eb655e7
Dornar <59340405+Ba12ley@users.noreply.github.com>
2024-03-24 10:03:51 +0000

    Initial Commit

---
+++ PotholesNI/PotholesNIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PotholesNIApp.swift
+//  PotholesNI
+//
+//  Created by Adam Baizley on 24/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct PotholesNIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
